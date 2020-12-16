da967bf6cf8f522c6b4f1ae372d2e9ff3761034d
Leonel-I <christian.inamagua58@student.qcc.cuny.edu>
2020-12-16 15:55:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zeksbe-rmyz6n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zeksbe-rmyz6n)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Logo Design',
+    price: 799,
+    description: 'A logo designed for your brand or company'
+  },
+  {
+    name: 'Cover Art',
+    price: 699,
+    description: 'Cover art for an album or song'
+  },
+  {
+    name: 'Event Flyer',
+    price: 299,
+    description: 'Flyer or poster designed for an event'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
